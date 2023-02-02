9e2dfdb411f41427c4b9808878230f305f1f55e2
Jon Sadowski <jwsadowski@gmail.com>
2023-02-02 12:31:12 -0600

    new commit

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,12 +1,16 @@
 import React from "react";
 
-const projectCards = {width: "18rem"}
+const projectCards = {width: "500px"}
 
 function Projects() {
     return (
         <>
-            <div className="card" style={projectCards}>
-                <img src="https://raw.githubusercontent.com/megsra17/movie-soundtrack-finder/main/Screen%20Shot%202022-10-18%20at%205.46.00%20PM.png" className="card-img-top" alt=" " />
+            <div className="card">
+                <img 
+                src="https://raw.githubusercontent.com/megsra17/movie-soundtrack-finder/main/Screen%20Shot%202022-10-18%20at%205.46.00%20PM.png" 
+                className="card-img-top" 
+                alt=" " 
+                style={projectCards}/>
                     <div className="card-body">
                         <h5 className="card-title">Movie Soundtrack Finder</h5>
                         <p className="card-text">An app that lets you instantly find a Movie or TV show's soundtrack</p>
@@ -14,8 +18,12 @@ function Projects() {
                     </div>
             </div>
 
-            <div className="card" style={projectCards}>
-                <img src="https://raw.githubusercontent.com/ddwk21/we-do/main/public/assets/homepage.JPG" className="card-img-top" alt=" " />
+            <div className="card">
+                <img 
+                src="https://raw.githubusercontent.com/ddwk21/we-do/main/public/assets/homepage.JPG" 
+                className="card-img-top" 
+                alt=" "
+                style={projectCards} />
                     <div className="card-body">
                         <h5 className="card-title">We Do</h5>
                         <p className="card-text">An app that lets you find a partner to do a select activity with</p>
@@ -23,8 +31,12 @@ function Projects() {
                     </div>
             </div>
 
-            <div className="card" style={projectCards}>
-                <img src="https://raw.githubusercontent.com/ddwk21/we-do/main/public/assets/homepage.JPG" className="card-img-top" alt=" " />
+            <div className="card">
+                <img 
+                src="https://raw.githubusercontent.com/ddwk21/we-do/main/public/assets/homepage.JPG" 
+                className="card-img-top" 
+                alt=" "
+                style={projectCards} />
                     <div className="card-body">
                         <h5 className="card-title">Text Editor</h5>
                         <p className="card-text">A text editor app</p>
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Resume() {
     return (
-        <section className="mt-5">
-            <div className="mt-5">
+        <section className="resume-section">
+            <div className="resume-div">
                 <a href="https://docs.google.com/document/d/1ctI2WKW9cdHTY9AO8aMroNpVNIDjG2YSYtu5e_3RjZE/edit">Visit my Github page</a>
             </div>
         </section>
