f90d7b933592de3e9dfbeba26924d8e10dd08b97
Jon Sadowski <jwsadowski@gmail.com>
2023-02-02 16:56:17 -0600

    working with css files

--- src/components/pages/Aboutme.js
+++ src/components/pages/Aboutme.js
@@ -4,7 +4,8 @@ function Aboutme() {
     return (
         <section className="about-section">
             <div className="about-div">
-                <p>Jon is a student of the UW-Wisconsin system's fullstack developer bootcamp. He has acquired skills in HTML/CSS, Javascript, Node.js, Express, SQL, Mongodb, REACT, and MERN stack. </p>
+                <p>Jon is a student of the UW-Wisconsin system's fullstack developer bootcamp. 
+                   He has acquired skills in HTML/CSS, Javascript, Node.js, Express, SQL, Mongodb, REACT, and MERN stack. </p>
             </div>
         </section>
     );
